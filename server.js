a8a0e9ad7b719ed4e694527fa3824c87ba222282
DRGHAM100 <drghamdakhol@gmail.com>
2022-09-09 19:20:03 +0300

    add cart page, auth

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
-    res.render('home');
-});
 
 app.use(express.static('public'));
 
@@ -15,6 +12,24 @@ app.use(express.static('public'));
 app.use(expressLayouts);
 app.set('views',path.join(__dirname,'resources/views'));
 app.set('view engine','ejs');
+app.set('layout','./layouts/main');
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+});
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+});
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+});
 
 
 
