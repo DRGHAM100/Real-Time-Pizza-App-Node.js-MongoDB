7c019304d2f76f71b4081212797e039bcb4812b4
DRGHAM100 <drghamdakhol@gmail.com>
2022-09-09 15:31:33 +0300

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+
+app.use(expressLayouts);
+app.set('views',path.join(__dirname,'resources/views'));
+app.set('view engine','ejs');
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port: ${PORT}`);
+});
